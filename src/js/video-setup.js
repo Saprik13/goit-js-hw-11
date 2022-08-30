400e51457177825c078afe1324f67f1042787127
Petro Sapronov <saprik13@gmail.com>
2022-08-30 10:03:48 +0300

    2

--- src/js/video-setup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("autoplay").addEventListener("click", function() {
-    document.querySelector(".video-setup").play();
-});
-
-document.getElementById("close-video").addEventListener("click", function () {
-    document.querySelector(".video-setup").pause();
-});
